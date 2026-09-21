e7c4eef7ab85975579329e3584546403ed51b400
Yuliia Miller <yulia.miller122@gmail.com>
2026-09-21 18:02:41 +0300

    fix paths for GitHub Pages

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,10 +1,18 @@
 import fileInclude from "gulp-file-include";
 import htmlmin from "gulp-htmlmin";
+import replace from "gulp-replace";
 
 export const html = () => {
   return app.gulp
     .src(app.path.src.html)
     .pipe(fileInclude())
+    .pipe(replace("../files/", "./files/"))
+    .pipe(replace("../index.html", "./index.html"))
+    .pipe(replace("../about_page.html", "./about_page.html"))
+    .pipe(replace("../services_page.html", "./services_page.html"))
+    .pipe(replace("../team_page.html", "./team_page.html"))
+    .pipe(replace("../publications_page.html", "./publications_page.html"))
+    .pipe(replace("../contact_page.html", "./contact_page.html"))
     .pipe(
       htmlmin({
         removeComments: true,
