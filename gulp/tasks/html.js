87d4c9ae812c02a7b3a243fe11c4875bdbb34b9a
Yuliia Miller <yulia.miller122@gmail.com>
2026-09-23 20:46:46 +0300

    add some corrections to html.js

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -10,8 +10,16 @@ export const html = () => {
     .pipe(replace("../index.html", "./index.html"))
     .pipe(replace("../about_page.html", "./about_page.html"))
     .pipe(replace("../services_page.html", "./services_page.html"))
+    .pipe(replace("../services-entry_page.html", "./services-entry_page.html"))
     .pipe(replace("../team_page.html", "./team_page.html"))
+    .pipe(replace("../team-entry_page.html", "./team-entry_page.html"))
     .pipe(replace("../publications_page.html", "./publications_page.html"))
+    .pipe(
+      replace(
+        "../publication-entry_page.html",
+        "./publication-entry_page.html",
+      ),
+    )
     .pipe(replace("../contact_page.html", "./contact_page.html"))
     .pipe(
       htmlmin({
